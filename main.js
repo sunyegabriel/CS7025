e561e16a656d1e702602f606d79a3669a4b5c1e3
Ye Sun <yesun@ITSERV.SCSS.TCD.IE>
2025-12-11 21:21:29 +0000

    Update: added some comments and notes

--- main.js
+++ main.js
@@ -1,25 +1,27 @@
 // 依旧假设HTML里有 <p id="contact-feedback"></p> 
 
-// 唯一在外面获取的可能就是这个按钮
+// 唯一在外面获取的可能就是这个按钮 The only thing that might be accessible externally is this button
 var sendButton = document.getElementById('sendButton');
 
-// 不检查按钮存不存在，直接用！
-// (如果页面上没有 'sendButton'，这行会在控制台报错，这很“初学者”)
+// Attention，这里本应去检查按钮是否存在，不过这里在代码中已经提前设了按钮，即id="sendButton"，注意这一点至关重要
+// (如果页面上没有 'sendButton'，这行会在控制台报错)
+// As demonstrated here, the button has already been defined in the code with id="sendButton". 
+// It is crucial to note that if there is no ‘sendButton’ on the page, this line will throw an error in the console.
 sendButton.onclick = function() {
   
-  // -- 把所有获取元素的代码都塞到函数里面 --
-  // (每次点击按钮都会重新获取一次，效率很低，但很“人类”)
+  // -- 把所有获取元素的代码都塞到函数里面 Put all the code for retrieving elements into a function --
+  // (这样每次点击按钮都会重新获取一次)
   var nameInput = document.getElementById('name');
   var emailInput = document.getElementById('email');
   var messageInput = document.getElementById('text');
   var feedback = document.getElementById('contact-feedback');
 
-  // 这里将值取出来
+  // 这里将值取出来 Fetch
   var name = nameInput.value;
   var email = emailInput.value;
   var message = messageInput.value;
 
-  // 在这里进行一个一个地检查
+  // 在这里进行一个一个地检查 Check one by one
   if (name == "") {
     feedback.innerHTML = "name is empty";
     feedback.style.color = "red";
@@ -32,17 +34,17 @@ sendButton.onclick = function() {
     feedback.innerHTML = "message is empty";
     feedback.style.color = "red";
   }
-  // 检查 @
+  // 检查，注意不要忽略 @ (Important)
   else if (email.indexOf('@') == -1) {
     feedback.innerHTML = "email is invalid";
     feedback.style.color = "red";
   } 
-  // 都通过了
+  // 都通过了 All pass
   else {
     feedback.innerHTML = "Thank " + "you " + name + "!";
     feedback.style.color = "green";
 
-    // 清空
+    // 清空 Clear
     nameInput.value = "";
     emailInput.value = "";
     messageInput.value = "";
